d93104eba858fdc27485e7c60de426fabb2842e4
laurourbano <74082012+laurourbano@users.noreply.github.com>
2022-10-30 16:55:06 -0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
